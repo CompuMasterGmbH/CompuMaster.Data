fc7902e45340ff60e385be0af836b2684dc72bd5
Jochen Wezel <jwezel@compumaster.de>
2015-11-25 13:53:47 +0100

    Allow tests for friend methods

--- CompuMaster.Test.Tools.Data/AssemblyInfo.vb
+++ CompuMaster.Test.Tools.Data/AssemblyInfo.vb
@@ -2,31 +2,14 @@ Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' Allgemeine Informationen über eine Assembly werden über die folgende 
-' Attributgruppe gesteuert. Ändern Sie diese Attributwerte, um Informationen,
-' die mit einer Assembly verknüpft sind, zu bearbeiten.
+<Assembly: AssemblyTitle("CompuMaster.Data.Test - MIT license edition")>
+<Assembly: AssemblyDescription("NUnit Test Lib")>
+<Assembly: AssemblyCompany("CompuMaster GmbH")>
+<Assembly: AssemblyProduct("CompuMaster Tools")>
+<Assembly: AssemblyCopyright("2005-2015 CompuMaster GmbH")>
+<Assembly: AssemblyTrademark("CompuMaster")>
 
-' Die Werte der Assemblyattribute überprüfen
-
-<Assembly: AssemblyTitle("")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("")> 
-<Assembly: AssemblyCopyright("")> 
-<Assembly: AssemblyTrademark("")> 
-<Assembly: CLSCompliant(True)> 
-
-'Die folgende GUID ist für die ID der Typbibliothek, wenn dieses Projekt in COM angezeigt wird
+<Assembly: CLSCompliant(True)>
 <Assembly: Guid("D8D4C425-B7A2-4F73-A91B-7C1EA686B0F6")>
 
-' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-'
-'      Haupversion
-'      Nebenversion 
-'      Buildnummer
-'      Revisionsnummer
-'
-' Sie können alle Werte angeben oder auf die standardmäßigen Build- und Revisionsnummern 
-' zurückgreifen, indem Sie '*' wie unten angezeigt verwenden:
-
-<Assembly: AssemblyVersion("2.8.2015.1020")>
+<Assembly: AssemblyVersion("2.8.2015.1125")>
