0c45f3d871c3058bec2140d12d5ad2549f77ab36
Jochen Wezel <jwezel@compumaster.de>
2019-03-12 17:22:32 +0100

    extended excel file format tests

--- CompuMaster.Test.Tools.Data/AssemblyTestEnvironment.vb
+++ CompuMaster.Test.Tools.Data/AssemblyTestEnvironment.vb
@@ -9,4 +9,20 @@
         Return System.IO.Path.Combine(TestDirectory(), relativePath)
     End Function
 
+    Public Shared Function TestFileAbsolutePath(relativePath As String, ensureDirectoryExists As Boolean, deleteExistingTargetFileIfExists As Boolean) As String
+        Dim Result As String = TestFileAbsolutePath(relativePath)
+        If ensureDirectoryExists Then
+            Dim DirName As String = System.IO.Path.GetDirectoryName(Result)
+            If System.IO.Directory.Exists(DirName) = False Then
+                System.IO.Directory.CreateDirectory(DirName)
+            End If
+        End If
+        If deleteExistingTargetFileIfExists Then
+            If System.IO.File.Exists(Result) = True Then
+                System.IO.File.Delete(Result)
+            End If
+        End If
+        Return Result
+    End Function
+
 End Class
--- CompuMaster.Test.Tools.Data/XlsReader.vb
+++ CompuMaster.Test.Tools.Data/XlsReader.vb
@@ -33,26 +33,41 @@ Namespace CompuMaster.Test.Data
         <Test()> Public Sub ReadFormatExcel95()
             Dim TestFile As String = AssemblyTestEnvironment.TestFileAbsolutePath("testfiles\test_for_lastcell_e50aka95.xls")
             CompuMaster.Data.XlsReader.ReadDataTableFromXlsFile(TestFile, "test")
+            TestFile = AssemblyTestEnvironment.TestFileAbsolutePath("app_data\xls95.xls", True, True)
+            CompuMaster.Data.DatabaseManagement.CreateMsExcelFile(TestFile, CompuMaster.Data.DatabaseManagement.MsExcelFileType.MsExcel95Xls)
+            CompuMaster.Data.XlsReader.ReadDataTableFromXlsFile(TestFile, "Table1")
         End Sub
 
         <Test()> Public Sub ReadFormatExcel97()
             Dim TestFile As String = AssemblyTestEnvironment.TestFileAbsolutePath("testfiles\test_for_lastcell_e70aka97-2003.xls")
             CompuMaster.Data.XlsReader.ReadDataTableFromXlsFile(TestFile, "test")
+            TestFile = AssemblyTestEnvironment.TestFileAbsolutePath("app_data\xls97.xls", True, True)
+            CompuMaster.Data.DatabaseManagement.CreateMsExcelFile(TestFile, CompuMaster.Data.DatabaseManagement.MsExcelFileType.MsExcel97Xls)
+            CompuMaster.Data.XlsReader.ReadDataTableFromXlsFile(TestFile, "Table1")
         End Sub
 
         <Test()> Public Sub ReadFormatExcel2007xlsx()
             Dim TestFile As String = AssemblyTestEnvironment.TestFileAbsolutePath("testfiles\test_for_lastcell_e12aka2007.xlsx")
             CompuMaster.Data.XlsReader.ReadDataTableFromXlsFile(TestFile, "test")
+            TestFile = AssemblyTestEnvironment.TestFileAbsolutePath("app_data\xls207.xlsx", True, True)
+            CompuMaster.Data.DatabaseManagement.CreateMsExcelFile(TestFile, CompuMaster.Data.DatabaseManagement.MsExcelFileType.MsExcel2007Xlsx)
+            CompuMaster.Data.XlsReader.ReadDataTableFromXlsFile(TestFile, "Table1")
         End Sub
 
         <Test()> Public Sub ReadFormatExcel2007xlsb()
             Dim TestFile As String = AssemblyTestEnvironment.TestFileAbsolutePath("testfiles\test_for_lastcell_e12aka2007.xlsb")
             CompuMaster.Data.XlsReader.ReadDataTableFromXlsFile(TestFile, "test")
+            TestFile = AssemblyTestEnvironment.TestFileAbsolutePath("app_data\xls207.xlsb", True, True)
+            CompuMaster.Data.DatabaseManagement.CreateMsExcelFile(TestFile, CompuMaster.Data.DatabaseManagement.MsExcelFileType.MsExcel2007Xlsb)
+            CompuMaster.Data.XlsReader.ReadDataTableFromXlsFile(TestFile, "Table1")
         End Sub
 
         <Test()> Public Sub ReadFormatExcel2007xlsm()
             Dim TestFile As String = AssemblyTestEnvironment.TestFileAbsolutePath("testfiles\test_for_lastcell_e12aka2007.xlsm")
             CompuMaster.Data.XlsReader.ReadDataTableFromXlsFile(TestFile, "test")
+            TestFile = AssemblyTestEnvironment.TestFileAbsolutePath("app_data\xls207.xlsm", True, True)
+            CompuMaster.Data.DatabaseManagement.CreateMsExcelFile(TestFile, CompuMaster.Data.DatabaseManagement.MsExcelFileType.MsExcel2007Xlsm)
+            CompuMaster.Data.XlsReader.ReadDataTableFromXlsFile(TestFile, "Table1")
         End Sub
 #End If
 
