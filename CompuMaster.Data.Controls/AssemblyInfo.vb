c9b2da5acd814c245bd52f07be8365e8e83cd4de
Jochen Wezel <jwezel@compumaster.de>
2019-03-20 18:25:38 +0100

    published new release to nuget

--- CompuMaster.Data.Controls/AssemblyInfo.vb
+++ CompuMaster.Data.Controls/AssemblyInfo.vb
@@ -5,7 +5,7 @@ Imports System.Runtime.CompilerServices
 
 <Assembly: AssemblyTitle("CompuMaster.Data.Controls - MIT license edition")>
 <Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("CompuMaster GmbH")> 
+<Assembly: AssemblyCompany("CompuMaster GmbH")>
 <Assembly: AssemblyProduct("CompuMaster Tools")>
 <Assembly: AssemblyCopyright("2005-2019 CompuMaster GmbH")>
 <Assembly: AssemblyTrademark("CompuMaster")> 
@@ -16,7 +16,7 @@ Imports System.Runtime.CompilerServices
 <Assembly: Guid("FDE425FC-D944-4747-A1D8-082356335D53")>
 
 'HINT: ALWAYS UPDATE AT PROJECTS CM.DATA + CM.DATA.CONTROLS TO THE VERY SAME VERSION NO.
-<Assembly: AssemblyVersion("2.8.2019.0314")>
+<Assembly: AssemblyVersion("2.8.2019.0320")>
 '<Assembly: AssemblyInformationalVersion("2.8.2019.0312a-beta1")>
 <Assembly: AssemblyDelaySign(False)>
 
